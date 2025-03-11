5755b142bea801070c9ee6111c3df0c6f8fbeef8
yasiqb89 <mian.yassir@gmail.com>
2025-03-12 00:30:55 +0100

    test add, test divide

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def get_weather(temp):
     else:
         return "cold"
 
+def add(a, b):
+    return a + b 
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b 
--- test_main.py
+++ test_main.py
@@ -1,6 +1,17 @@
-from main import get_weather
+from main import get_weather, add, divide
+import pytest
 
 def test_get_weather():
     assert get_weather(21) == "hot" # condition needs to be true to pass
+    assert get_weather(10) == "cold" 
+    assert get_weather(30) == "hot"
 
     
+def test_add():
+    assert add(2, 3) == 5, "2 + 3 should be 5"
+    assert add(-1, 1) == 0, " -1 + 1 should be 0"
+    assert add(0, 0) == 0, "0 + 0 should be 0"
+
+def test_divide():
+    with pytest.raises(ValueError, match = "Cannot divide by zero"):
+        divide(10, 0)
